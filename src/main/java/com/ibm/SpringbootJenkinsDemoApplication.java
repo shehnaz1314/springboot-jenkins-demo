0cae538457ad4f2abf1f9ec893df9811183ad615
shehnaz1314 <Shehnaz.A@ibm.com>
2026-05-15 14:42:08 +0530

    Refactor SpringbootJenkinsDemoApplication class
    Removed SpringBootServletInitializer extension and related method.

--- src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
+++ src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
@@ -4,13 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJenkinsDemoApplication  extends SpringBootServletInitializer{
-
-	
-  @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
-    }
+public class SpringbootJenkinsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
