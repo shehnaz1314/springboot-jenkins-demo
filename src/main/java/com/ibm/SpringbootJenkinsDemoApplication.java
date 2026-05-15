910554868c6b8423548189d60660fd31a011d5af
shehnaz1314 <Shehnaz.A@ibm.com>
2026-05-15 11:47:18 +0530

    Remove unnecessary console output

--- src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
+++ src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
@@ -9,8 +9,6 @@ public class SpringbootJenkinsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
 		System.out.println("Git with Jenkins Demo....");
-		System.out.println("Checked!!!!");
-		
 	}
 
 }
