e4da49341c5aa772a94a749d7a878b48a074275d
shehnaz1314 <Shehnaz.A@ibm.com>
2026-05-15 14:23:07 +0530

    Extend application class for servlet support
    Extend SpringbootJenkinsDemoApplication to support servlet initialization and update main method output.

--- src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
+++ src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJenkinsDemoApplication {
+public class SpringbootJenkinsDemoApplication  extends SpringBootServletInitializer{
+
+	
+  @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
 		System.out.println("Git with Jenkins Demo....");
-		System.out.println("Checked!!!!!!!");
 	}
 
 }
