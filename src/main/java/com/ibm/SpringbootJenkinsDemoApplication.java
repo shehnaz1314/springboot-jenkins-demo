ddcb60f19ff6e0a6fe34481871420f123fd82acf
shehnaz1314 <Shehnaz.A@ibm.com>
2026-05-15 11:49:08 +0530

    Add a confirmation message to main method

--- src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
+++ src/main/java/com/ibm/SpringbootJenkinsDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringbootJenkinsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
 		System.out.println("Git with Jenkins Demo....");
+		System.out.println("Checked!!!!!!!");
 	}
 
 }
